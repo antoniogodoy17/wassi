f4327d73e90f0e8c509112ad1c2763e0948619c9
antoniogodoy17 <tonny-008@hotmail.com>
2019-10-03 19:23:44 -0700

    Created fake data from component

--- src/app/accounts/accounts.component.ts
+++ src/app/accounts/accounts.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accounts.component.sass']
 })
 export class AccountsComponent implements OnInit {
+	accounts = [
+		{
+			name: 'Platinum',
+			description: 'This is a fake description for an account.',
+			balance: '1200',
+			currency: 'MXN'
+		},
+		{
+			name: 'Black',
+			description: 'This is a fake description for an account.',
+			balance: '8000',
+			currency: 'MXN'
+		},
+		{
+			name: 'Gold',
+			description: 'This is a fake description for an account.',
+			balance: '-1200',
+			currency: 'USD'
+		},
+	];
+	constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
